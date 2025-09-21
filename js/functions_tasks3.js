2c6a466f909bc59866df9e49d20ea6e4a254aa17
Victoria <kochetit@gmail.com>
2025-09-21 16:31:03 +0300

    task 3.2 completed

--- js/functions_tasks3.js
+++ js/functions_tasks3.js
@@ -1,5 +1,7 @@
 "use strict";
 
+//Task 1
+
 function getTimeFromMinutes(num) {
     if ( typeof(num) !== "number" || !Number.isInteger(num) || num < 0){
         return `Ошибка, проверьте данные`;
@@ -20,3 +22,22 @@ return `Это ${hours} ${timeToShow} и ${mins} минут`;
 
 let num = +prompt("Enter minutes: ");
 alert(getTimeFromMinutes(num));
+
+//Task 2
+
+function findMaxNumber(num1, num2, num3, num4) {
+
+    if  (typeof(num1) !== 'number' ||
+        typeof(num2) !== 'number' ||
+        typeof(num3) !== 'number' ||
+        typeof(num4) !== 'number'){
+        return 0;
+    }
+const num = [num1, num2, num3, num4];
+return Math.max(...num);
+}
+let num1 = +prompt("Enter number 1: ");
+let num2 = +prompt("Enter number 2: ");
+let num3 = +prompt("Enter number 3: ");
+let num4 = +prompt("Enter number 4: ");
+alert(findMaxNumber(num1, num2, num3, num4));
